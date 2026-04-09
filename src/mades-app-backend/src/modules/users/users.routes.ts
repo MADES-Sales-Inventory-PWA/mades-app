373df7655109e02f61f7f43e3b3c4576f54f4a2b
leonardoavila18 <miguel.avila04@uptc.edu.co>
2026-04-09 17:25:12 -0500

    Revert "Refactor/user registration"

--- src/mades-app-backend/src/modules/users/users.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { UserController } from "./users.controller"
-
-const router = Router();
-const userController = new UserController();
-
-router.post("/", userController.createUser.bind(userController));
-
-export default router;
