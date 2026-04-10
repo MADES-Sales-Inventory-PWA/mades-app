4ceed857ef6a09a55b5b19a27b24e8c34a816d8a
JohanGil1502 <johan.gil@uptc.edu.co>
2026-04-10 14:30:09 -0500

    Add connection status indicator

--- src/mades-app-front/src/components/InputPassword.tsx
+++ src/mades-app-front/src/components/InputPassword.tsx
@@ -22,7 +22,7 @@ export const InputPassword = ({ label, type, placeholder, value, onChange }: Inp
           id={label}
           value={value}
           onChange={(event) => onChange(event.target.value)}
-          className="bg-input-login border border-input-border border-[1.5px] mt-1 h-13 px-2 block w-full rounded-default shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+          className="font-sans text-s text-gray-500 bg-input-login border border-input-border border-[1.5px] mt-1 h-13 px-2 block w-full rounded-default shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 placeholder:font-sans placeholder:text-gray-500 placeholder:italic"
           placeholder={placeholder}
         />
         <button type="button" onClick={() => setShow(!show)} className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500">
