d2c993bf9c0d160d5e59ea1ae4a86e9a21cbb5ac
JohanGil1502 <johan.gil@uptc.edu.co>
2026-04-10 10:26:30 -0500

    Revert changes in main.tsx file

--- src/mades-app-front/src/main.tsx
+++ src/mades-app-front/src/main.tsx
@@ -4,22 +4,15 @@ import './index.css'
 import App from './App.tsx'
 
 if ('serviceWorker' in navigator) {
-  if (import.meta.env.PROD) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .catch((error) => {
-          console.error('Error registrando service worker:', error)
-        })
-    })
-  } else {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      registrations.forEach((registration) => {
-        registration.unregister()
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .catch((error) => {
+        console.error('Error registrando service worker:', error)
       })
-    })
-  }
+  })
 }
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
